145184044e9a0c679836e165719a178018da2d9c
Zvonimir <zvonimirplivelic91@gmail.com>
2021-11-10 11:41:19 +0100

    Auto stash before rebase of "master"
    Created settings screen

--- app/src/main/java/com/zvonimirplivelic/pomodorotimer/activity/MainActivity.kt
+++ app/src/main/java/com/zvonimirplivelic/pomodorotimer/activity/MainActivity.kt
@@ -216,19 +216,18 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_settings -> {
+                val intent = Intent(this, SettingsActivity::class.java)
+                startActivity(intent)
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
